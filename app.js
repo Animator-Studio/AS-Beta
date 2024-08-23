f20a62b2c6e41f45f8e6bcd4deb4a2b36573e9fe
blazgocompany <96586789+blazgocompany@users.noreply.github.com>
2024-08-23 15:24:01 -0700

    Multiple changes

--- app.js
+++ app.js
@@ -40,6 +40,28 @@ app.get("/", (req, res) => {
       console.error(err);
       return;
     }
+    out = `
+    <!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <script type="text/javascript">
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "nrvoacr6ky");
+        </script>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Home</title>       
+          <link rel="stylesheet" type="text/css" href="/resources/home/styles.css">
+          <link rel="icon" type="image/x-icon" href="/images/favicon.png">
+      </head>
+      <body>
+      ${data}
+      </body>
+      </html>
+    `
     res.send(data);
   });
 });
